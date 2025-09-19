4af234047a9ef748d8f2f97381462562a4e357b1
mdasifalijihat <mdasifalijihat@gmail.com>
2025-09-19 22:17:03 +0600

    added loading

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,9 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function layout({children}) {
   return (
     <div>
-        <nav>about || navigation</nav>
+          {/* navbar  */}
+        <nav>
+          <ul className="flex gap-4 mb-4">
+            <li>
+                <Link href={"/about/mission"}> mission </Link>
+            </li>
+            <li>
+              <Link href={"/about/vission"}> Vission </Link>
+            </li>
+          </ul>
+        </nav>
       {children}
     </div>
   )
