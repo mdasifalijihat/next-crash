207490227cd223e8660e09a08ef083300b93789c
mdasifalijihat <mdasifalijihat@gmail.com>
2025-09-19 10:23:18 +0600

    added page and layout

--- app/layout.js
+++ app/layout.js
@@ -18,11 +18,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="p-6">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav> Home || About </nav>
         {children}
+        <footer> Footer </footer>
       </body>
     </html>
   );
